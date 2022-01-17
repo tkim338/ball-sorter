2f7f05d655e3231914f35b12279bc65221d04709
Thomas Kim <tjk103@gmail.com>
2022-01-16 20:03:22 -0500

    wrapped up loose ends

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import image_processor
+import puzzle_solver
 
 tube_dict = image_processor.process('/Users/tkim/Downloads/bs2.png')
 print(tube_dict)
 
+sol = puzzle_solver.solve(tube_dict)
+for move in sol:
+	print(move)
