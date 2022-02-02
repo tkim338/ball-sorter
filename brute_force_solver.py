d39a1561e0e2bf5661efa50d7482d97b1be294df
Thomas Kim <tjk103@gmail.com>
2022-02-01 23:16:17 -0500

    added permutation functions

--- brute_force_solver.py
+++ brute_force_solver.py
@@ -3,8 +3,57 @@ from puzzle_model import PuzzleModel
 import sys
 import random
 
-def equal_states(state0, state1):
-	return True
+def find_all(string, substring):
+	indices = list()
+	i = -1
+	while i < len(string):
+		index = string.find(substring, i+1)
+		if index > -1:
+			indices.append(index)
+			i = index
+		else:
+			break
+	return indices
+
+def find_char_sets(string):
+	unique_chars = list(set(string))
+	unique_chars.remove('*')
+	
+	char_list = list()
+	for char in unique_chars:
+		char_list.append(find_all(string, char))
+		
+	char_dict = dict()
+	for char in unique_chars:
+		char_dict[char] = char_list
+		
+	return char_dict, len(char_list)
+
+def get_permutation_list(index_list):
+	if len(index_list) <= 1:
+		return [index_list]
+	
+	permutations = list()
+	for i in range(0, len(index_list)):
+		sub_permutations = get_permutation_list(index_list[:i]+index_list[i+1:])
+		for sp in sub_permutations:
+			permutations.append([index_list[i]]+sp)
+	return permutations
+
+def get_permutations(string):
+	char_dict, index_len = find_char_sets(string)
+	index_permutations = get_permutation_list(list(range(0, index_len)))
+
+	permutations = list()
+	for index_list in index_permutations:
+		new_string = '*'*len(string)
+		for key in char_dict:
+			index = index_list.pop(0)
+			index_sets = char_dict[key]
+			for loc in index_sets[index]:
+				new_string = new_string[:loc]+key+new_string[loc+1:]
+		permutations.append(new_string)
+	return permutations
 
 def solve(model):
 	state_history = [model.state_set()]
