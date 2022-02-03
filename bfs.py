c8554f650448046df4a66cff990e780db9f53a52
Thomas Kim <tjk103@gmail.com>
2022-02-02 21:50:00 -0500

    refactored; bfs and dfs separate

--- bfs.py
+++ bfs.py
@@ -3,29 +3,6 @@ from puzzle_model import PuzzleModel
 import sys
 import random
 
-def dfs_search(model, state_history, path):
-	# state_history = [model.state_set()]
-	for i in range(0,100):
-		print(f'Paths checked: {i}.')
-
-		if model.state_set() not in state_history:
-			state_history.append(model.state_set())
-			options = model.get_options()
-			for option in options:
-				# attempt option
-				option_model = PuzzleModel(model.state)
-				option_model.process_move(option)
-				if option_model.win_state == True:
-					return True, state_history, path+[option]
-				else:
-					won, state_history, p = dfs_search(option_model, state_history, path+[option])
-					if won:
-						return True, state_history, p
-			# no options for this model
-		# model state set already checked
-	# path limit reached
-	return False, state_history, path
-
 def solve(model):
 	state_history = [model.state_set()]
 
@@ -80,18 +57,13 @@ def solve(model):
 	return 'solution not found'
 
 if __name__ == "__main__":
-	print(sys.argv[1])
 	image_loc = sys.argv[1]
 	tube_dict = image_processor.process(image_loc)
 	model = PuzzleModel(tube_dict)
-	print(model.bins)
-	print(model.win_state)
+	# print(model.bins)
+	# print(model.win_state)
 
-	# print(model.to_string())
-	# sol = solve(model)
-	# for move in sol:
-	# 	print(move)
-
-	win_state, history, sol = dfs_search(model, list(), list())
+	print(model.to_string())
+	sol = solve(model)
 	for move in sol:
-		print(move)
+		print(f'{move[0]+1}, {move[1]+1}')
---
+++ dfs.py
@@ -0,0 +1,39 @@
+import image_processor
+from puzzle_model import PuzzleModel
+import sys
+import random
+
+def dfs_search(model, state_history, path):
+	# state_history = [model.state_set()]
+	for i in range(0,100):
+		print(f'Paths checked: {i}.')
+
+		if model.state_set() not in state_history:
+			state_history.append(model.state_set())
+			options = model.get_options()
+			for option in options:
+				# attempt option
+				option_model = PuzzleModel(model.state)
+				option_model.process_move(option)
+				if option_model.win_state == True:
+					return True, state_history, path+[option]
+				else:
+					won, state_history, p = dfs_search(option_model, state_history, path+[option])
+					if won:
+						return True, state_history, p
+			# no options for this model
+		# model state set already checked
+	# path limit reached
+	return False, state_history, path
+
+if __name__ == "__main__":
+	image_loc = sys.argv[1]
+	tube_dict = image_processor.process(image_loc)
+	model = PuzzleModel(tube_dict)
+	# print(model.bins)
+	# print(model.win_state)
+
+	print(model.to_string())
+	win_state, history, sol = dfs_search(model, list(), list())
+	for move in sol:
+		print(f'{move[0]+1}, {move[1]+1}')
